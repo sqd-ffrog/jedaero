a2646af6cd9ea4ac37c557006666ac1676dbfba4
aerain <j40f893@gmail.com>
2019-08-22 16:25:27 +0900

    아이콘추가

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     "assets": [
-        "./images/jedaero_icon.png"
+        "./images/jedaero-splash-200x200.png",
+        "./images/jedaero-splash-400x400.png",
+        "./images/jedaero-splash-600x600.png",
+        "./images/jedaero.png",
+        "./images/jedaero-40x40.png",
+        "./images/jedaero-58x58.png",
+        "./images/jedaero-60x60.png",
+        "./images/jedaero-80x80.png",
+        "./images/jedaero-87x87.png",
+        "./images/jedaero-120x120.png",
+        "./images/jedaero-180x180.png",
     ]
 }
