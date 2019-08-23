f1300ea198f12bd41ec387d42430ee8b6ddb70dc
aerain <j40f893@gmail.com>
2019-08-23 18:04:07 +0900

    검색필터조건

---
+++ components/drawer/jnuservice/dreamy/LecturePlanDetail.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import { View, Text } from 'react-native';
+
+const LecturePlanDetail = () => {
+    return (
+        <View>
+            <Text>상세 ㅎㅇ</Text>
+        </View>
+    )
+}
+
+export default LecturePlanDetail;
