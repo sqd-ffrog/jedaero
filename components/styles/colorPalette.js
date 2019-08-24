cac1e989ee1ad12eb21aca829867b95ff9d69738
aerain <j40f893@gmail.com>
2019-08-24 18:49:16 +0900

    홈화면그림자

--- components/styles/colorPalette.js
+++ components/styles/colorPalette.js
@@ -3,7 +3,7 @@ const colorPalette = {
     textColor: '#000000',
     cardBackgroundColor: '#ffffff',
     cardBorderColor: '#e7e7e7',
-    backgroundColor: '#f7f7f7',
+    backgroundColor: '#ffffff',
     subTextColor: '#7d7d7d',
     smartCheckColor: '#ff6659'
 }
---
+++ tool/elevationShadow.js
@@ -0,0 +1,14 @@
+export default function elevationShadowStyle(elevation) {
+    return {
+      elevation,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 0.2 * elevation },
+      shadowOpacity: 0.3,
+      shadowRadius: 0.5 * elevation
+    };
+}
+
+/**
+ * https://stenbeck.io/styling-shadows-in-react-native-ios-and-android/
+ */
+
