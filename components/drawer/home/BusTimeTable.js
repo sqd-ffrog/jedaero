0f2d54df40a2994334f660694facb89099b0f34b
aerain <j40f893@gmail.com>
2019-10-24 15:41:05 +0900

    refactor BusSchedule

--- components/drawer/home/BusTimeTable.js
+++ components/drawer/home/BusTimeTable.js
@@ -1,58 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Platform, FlatList } from 'react-native'
-import Carousel, { Pagination } from 'react-native-snap-carousel';
+import React, {useState, useEffect, Fragment} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { mainScreen } from '../../styles/busStyle';
-import { foodMenuListStyles } from '../../styles/jedaeroCSS';
+import Swiper from 'react-native-swiper';
 import BusTb from '../../../jsons/busscheduleDetail.json';
 import colorPalette from '../../styles/colorPalette';
+import { foodMenuListStyles } from '../../styles/jedaeroCSS';
 
-const sliderWidth = 220;
-const itemWidth = 320;
+const BusTimeTable = () => {
+    const [routeA, setRouteA] = useState(null);
+    const [routeB, setRouteB] = useState(null);
 
-
-const BusSchedule = () => {
-    const [routeA, setRouteA] = useState(BusTb.routeName.A);
-    const [routeB, setRouteB] = useState(BusTb.routeName.B)
+    useEffect(() => {
+        setRouteA(BusTb.routeName.A);
+        setRouteB(BusTb.routeName.B);
+    }, []);
     return (
-        <View style={mainScreen.busScheduleMain}>
+        <Fragment>
             <View style={mainScreen.busScheduleContainer}>               
                 <View style={mainScreen.busScheduleName}>
                     <Text style={mainScreen.blockTitle}>A</Text>
                     <Text style={mainScreen.busWay}>반시계방향</Text>
                 </View>
-                <Carousel
-                    data={routeA}
-                    renderItem={_renderItem_A}
-                    // sliderWidth={sliderWidth}
-                    // itemWidth={itemWidth}
-                />
+                {routeA && (
+                    <Swiper {...swiperStyle}>
+                        {routeA.map(_renderItem_A)}
+                    </Swiper>
+                )}
+                
             </View>
             <View style={mainScreen.busScheduleContainer}>     
                 <View style={mainScreen.busScheduleName}>          
                     <Text style={mainScreen.blockTitle}>B</Text>
                     <Text style={mainScreen.busWay}>시계방향</Text>
                 </View>
-                <Carousel
-                    data={routeB}
-                    renderItem={_renderItem_B}
-                    sliderWidth={sliderWidth}
-                    itemWidth={itemWidth}
-                />
+                {routeB && (
+                    <Swiper {...swiperStyle}>
+                        {routeB.map(_renderItem_B)}
+                    </Swiper>
+                )}
+                
             </View>
-        </View>
-    );
+        </Fragment>
+    )
 }
+
+BusTimeTable.navigationOptions = () => ({
+    title: "버스시간표"
+})
 const _busrender_a = ({item, index}) => (
     <View style={foodMenuListStyles.container}>
         <Text> {index+1}회 </Text>
         <Text style={foodMenuListStyles.labelStyle}>{item}</Text>
     </View> 
 )
-const _renderItem_A = ({item, index}) => {
+const _renderItem_A = (item, index) => {
     // st = index
     return (
-        
-        <View style={{flex: 1, backgroundColor: colorPalette.backgroundColor, paddingHorizontal:10}}>
+        <View key={index} style={{flex: 1, paddingHorizontal:16}}>
             <Text style={mainScreen.blockTitle}>{item}</Text>
             <FlatList
                 contentContainerStyle={{flexDirection: 'column', justifyContent: 'space-between'}}
@@ -63,22 +67,16 @@ const _renderItem_A = ({item, index}) => {
         </View>
     );
 }
-//이거 해야댐
-const make_time = (time,index) =>{
-    let _time = time.split(':');
-    return _time[0]+':'+ (Number(_time[1])+index)
-}
+
 const _busrender_b = ({item, index}) => (
-        <View style={foodMenuListStyles.container}>
-            <Text> {index+1}회 </Text>
-            <Text style={foodMenuListStyles.labelStyle}>{item}</Text>
-        </View> 
+    <View style={foodMenuListStyles.container}>
+        <Text> {index+1}회 </Text>
+        <Text style={foodMenuListStyles.labelStyle}>{item}</Text>
+    </View> 
 )
-const _renderItem_B = ({item, index}) => {
-    st = index
+const _renderItem_B = (item, index) => {
     return (
-        
-        <View style={{flex: 1, backgroundColor: colorPalette.backgroundColor, paddingHorizontal:10}}>
+        <View key={index} style={{flex: 1, paddingHorizontal:10}}>
             <Text style={mainScreen.blockTitle}> {item} </Text>
             <FlatList
                 contentContainerStyle={{flexDirection: 'column', justifyContent: 'space-between'}}
@@ -90,4 +88,23 @@ const _renderItem_B = ({item, index}) => {
         
     );
 }
-export default BusSchedule;
+
+const styles = {
+    swiperStyle: {
+        paddingTop: 8
+    },
+    swiperContainerStyle: {
+        overflow: 'hidden'
+        
+    }
+}
+const swiperStyle = {
+    showsPagination: false,
+    loadMinimal: true,
+    loadMinimalSize: 1,
+    loadMinimalLoader: (<Fragment/>),
+    style: styles.swiperStyle,
+    containerStyle: styles.swiperContainerStyle
+}
+
+export default BusTimeTable;
